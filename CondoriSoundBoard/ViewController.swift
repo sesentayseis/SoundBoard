4fa2b87e4054ef07d5308040fc863b41d887ebf4
sesentayseis <carlos.condori.c@tecsup.edu.pe>
2023-05-24 10:44:56 -0500

    Initial Commit

---
+++ CondoriSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CondoriSoundBoard
+//
+//  Created by Mac 05 on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
